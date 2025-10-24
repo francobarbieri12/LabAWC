818c82c222c9120d5a76b95cdcb7d3a562b70f87
Kalilalee <kalilale.fernandez@outlook.com>
2025-10-24 01:21:25 -0300

    ADD: File: componentes with cards.js
    - Se agrego la carpeta componentes para las cards de los productos

---
+++ src/componentes/cards.js
@@ -0,0 +1,23 @@
+import { getProducts } from "../js/api.js";
+
+export function RenderCards(){
+  let productList = document.querySelector('#product-list')
+  getProducts().then((products)=> {
+  let template = '';
+  products.forEach(p => {
+    template += `
+    <div class="col">
+    <div class="card">
+      <img src="${p.imagen}" class="card-img-top" alt="${p.title}">
+      <div class="card-body">
+        <h5 class="card-title">${p.title}</h5>
+        <p class="card-text">${p.description}</p>
+      </div>
+    </div>
+    </div>
+    `;
+  });
+  productList.innerHTML = template;
+
+}); 
+}
